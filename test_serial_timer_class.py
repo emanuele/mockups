7d002d48e42b76acfc93da2c5425e5db188f2f13
Emanuele Olivetti <olivetti@fbk.eu>
2014-07-23 09:58:10 +0200

    added code to test the SerialTimer class

---
+++ test_serial_timer_class.py
@@ -0,0 +1,35 @@
+import time
+from serial_timer import SerialTimer
+from fake_serial import Serial
+from time import sleep
+# import sys
+
+
+def mytime():
+    return time.time() * 1000.0
+
+
+if __name__ == '__main__':
+
+    verbose = False
+    
+    # f = open('log.txt', 'w')
+    # sys.stdout = f
+
+    ser = Serial(values=['1','2','5','5','5','5','5','5','5','5'], timeout=2.0, seed=1, verbose=verbose)
+    ser.open()
+
+    print("Parent: Creating SerialTimer.")
+    st = SerialTimer(t0=mytime(), ser=ser, time=mytime, good=['1','2'], bad=['5'], total_time=2000.0, flush=True, verbose=verbose)
+
+    for i in range(10):
+        print("Parent: iteration %d" % i)
+        st.start_timer()
+        print("Parent: waiting some seconds.")
+        sleep(4)
+        key, elapsed_time = st.get_timing()
+        print("Key pressed: %s , after %sms." % (key, elapsed_time))
+        print("")
+    
+    st.stop()
+    
